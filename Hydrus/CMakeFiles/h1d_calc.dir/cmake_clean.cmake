4da19244713270e41cc373f98a7ef672583d49e7
HeymansAdrien <36161856+HeymansAdrien@users.noreply.github.com>
2021-10-13 13:09:26 +0200

    Proc annexes

---
+++ Hydrus/CMakeFiles/h1d_calc.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/h1d_calc.dir/HYDRUS.FOR.o"
+  "CMakeFiles/h1d_calc.dir/HYSTER.FOR.o"
+  "CMakeFiles/h1d_calc.dir/INPUT.FOR.o"
+  "CMakeFiles/h1d_calc.dir/MATERIAL.FOR.o"
+  "CMakeFiles/h1d_calc.dir/OUTPUT.FOR.o"
+  "CMakeFiles/h1d_calc.dir/SINK.FOR.o"
+  "CMakeFiles/h1d_calc.dir/SOLUTE.FOR.o"
+  "CMakeFiles/h1d_calc.dir/TEMPER.FOR.o"
+  "CMakeFiles/h1d_calc.dir/TIME.FOR.o"
+  "CMakeFiles/h1d_calc.dir/WATFLOW.FOR.o"
+  "h1d_calc.pdb"
+  "h1d_calc"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang Fortran)
+  include(CMakeFiles/h1d_calc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
